3f881f879987afd4e3b959ad547548151ab45d0a
alesuuuuk <114850560+alesuuuuk@users.noreply.github.com>
2023-10-31 17:33:32 +0200

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,60 @@
-const filterInput=  (name)=> {
-    name.forEach(i => {
-        console.log(i)
+document.addEventListener("DOMContentLoaded", () => {
+    // task one 
+    const nameInput = document.getElementById("nameInput");
+    nameInput.addEventListener("input", (event) => {
+        const inputValue = nameInput.value;
+        const filteredValue = inputValue.replace(/\d/g, "");
+        if (inputValue !== filteredValue) {
+            nameInput.value = filteredValue;
+        }
     });
-}
+    // task two
+    const openButton = document.getElementById("openButton");
+            const modal = document.getElementById("modal");
+            const closeButton = document.getElementById("closeButton");
 
+            openButton.addEventListener("click", function () {
+                modal.style.display = "block";
+            });
 
-document.addEventListener("DOMContentLoaded", () =>{
-    // task one
-    const FORM_INPUT_NAME=  document.querySelector("#form__name")
-    console.log(FORM_INPUT_NAME)
-    FORM_INPUT_NAME.addEventListener("submit", (e)=>{
-        e.preventDefault()
-        const USER_NAME= document.querySelector("#nameInput").value
+            closeButton.addEventListener("click", function () {
+                modal.style.display = "none";
+            });
+        
+    // task three
+    function moveBall(event) {
+        const ball = document.getElementById("football");
+        const field = document.querySelector(".football-field");
 
-        filterInput(USER_NAME)
-    })
-})
+        // Отримуємо координати кліку відносно поля
+        const clickX = event.clientX - field.getBoundingClientRect().left;
+        const clickY = event.clientY - field.getBoundingClientRect().top;
+
+        // Розраховуємо нові координати для м'яча
+        const ballX = clickX - ball.clientWidth / 2;
+        const ballY = clickY - ball.clientHeight / 2;
+
+        // Перевірка, щоб м'яч не виходив за межі поля
+        if (ballX < 0) ballX = 0;
+        if (ballY < 0) ballY = 0;
+        if (ballX > field.clientWidth - ball.clientWidth) ballX = field.clientWidth - ball.clientWidth;
+        if (ballY > field.clientHeight - ball.clientHeight) ballY = field.clientHeight - ball.clientHeight;
+
+        // Змінюємо позицію м'яча з плавною анімацією
+        ball.style.transform = `translate(${ballX}px, ${ballY}px)`;
+    }
+   
+    
+    // task five 
+    function toggleSelected(book) {
+        const books = document.querySelectorAll('.book');
+
+        // Знімаємо виділення з усіх книг
+        for (const item of books) {
+            item.classList.remove('selected');
+        }
+
+        // Встановлюємо виділення для клікнутої книги
+        book.classList.add('selected');
+    }
+});
